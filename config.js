46022384c9b922d8f20ab78d23e23d191c13ea46
底层用户 <ytqz104ban@126.com>
2021-11-20 11:43:58 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,40 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'IMSYY 站点监测',
+
+  // 站点链接
+  SiteUrl: 'https://status.imsyy.top/',
+
+  // UptimeRobot Api 域名
+  // 只需填写域名部分，默认为官网域名
+  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
+  // 详见 https://github.com/yb/uptime-status/ 说明
+    ApiDomain: 'status.imsyy.workers.dev',
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur1022423-1019cd0e942707cf21d80847',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: true,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+      {
+          text: '主页',
+          url: 'https://imsyy.top/'
+      },
+      {
+          text: '博客',
+          url: 'https://blog.imsyy.top/'
+      }
+  ]
+};
