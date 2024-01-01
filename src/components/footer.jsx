8209d9a2ad8b07a18c21af60d116582201775b0f
buhuizhemelangbei <153802970+buhuizhemelangbei@users.noreply.github.com>
2024-01-01 18:36:54 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -34,9 +34,9 @@ const Footer = () => {
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          Copyright&nbsp;&copy;&nbsp;2023&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://www.imsyy.top/" text="超越" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
