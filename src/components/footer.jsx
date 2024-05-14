9b0eeeeb54e1bf1317451804863fb47fa2c9e75a
Xinnn <111115403+2259001833@users.noreply.github.com>
2024-05-14 14:23:02 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,6 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
