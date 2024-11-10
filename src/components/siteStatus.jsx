ef3b9cf348b65b5e29e8191b4c701b350b3d3a33
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-11-10 13:11:52 +0800

    Update siteStatus.jsx

--- src/components/siteStatus.jsx
+++ src/components/siteStatus.jsx
@@ -1,30 +1,88 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 import { formatNumber, formatDuration } from "@/utils/timeTools";
 import { LinkTwo } from "@icon-park/react";
-import { Tooltip, Button, Result, Modal } from "antd";
+import { Tooltip, Button, Result, Modal, Input, message } from "antd";
 import CustomLink from "@/components/customLink";
 import SiteCharts from "@/components/siteCharts";
 
 const SiteStatus = ({ siteData, days, status }) => {
-  // 弹窗数据
   const [siteDetailsShow, setSiteDetailsShow] = useState(false);
   const [siteDetailsData, setSiteDetailsData] = useState(null);
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [passwordInput, setPasswordInput] = useState("");
 
-  // 是否显示链接
   const isShowLinks = import.meta.env.VITE_SHOW_LINKS === "true";
+  const correctPassword = import.meta.env.VITE_PASSWORD;
 
-  // 开启弹窗
+  useEffect(() => {
+    const authorized = localStorage.getItem("isAuthorized");
+    if (authorized === "true") {
+      setIsAuthorized(true);
+    }
+  }, []);
+
+  const handlePasswordSubmit = () => {
+    if (passwordInput === correctPassword) {
+      setIsAuthorized(true);
+      localStorage.setItem("isAuthorized", "true");
+    } else {
+      message.error("密码错误，请重试");
+    }
+  };
+
   const showSiteDetails = (data) => {
     setSiteDetailsShow(true);
     setSiteDetailsData(data);
   };
 
-  // 关闭弹窗
   const closeSiteDetails = () => {
     setSiteDetailsShow(false);
     setSiteDetailsData(null);
   };
+  if (correctPassword) {
+  if (!isAuthorized) {
+    return (
+      <Modal
+        title="请输入密码"
+        open={!isAuthorized}
+        footer={null}
+        closable={false}
+        centered
+        style={{ width: "520px" }} // 设置弹窗宽度
+      >
+        <Input.Password
+          placeholder="输入密码"
+          value={passwordInput}
+          onChange={(e) => setPasswordInput(e.target.value)}
+          onPressEnter={handlePasswordSubmit}
+          addonAfter={
+            <Button
+              type="primary"
+              onClick={handlePasswordSubmit}
+              style={{
+                height: "30px",
+              }}
+            >
+              提交
+            </Button>
+          }
+          style={{
+            borderRadius: "6px", // 输入框圆角
+          }}
+        />
+        <style>
+          {`
+            .ant-input-group-addon {
+              background-color: rgba(0, 0, 0, 0%) !important;
+              border: 1px solid rgba(217, 217, 0, 0) !important;
+              padding: 0px 0px 0px 13px !important;
+            }
+          `}
+        </style>
+      </Modal>
+    );
+  }}
 
   return (
     <SwitchTransition mode="out-in">
