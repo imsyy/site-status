c86809c67e17714654b9584eeadb9c6f651ebb0e
LddsnHong <1340723643@qq.com>
2024-11-25 03:46:33 +0800

    什么事都没发生

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <p>
           <CustomLink
             text={Package.alia}
-            to="https://github.com/imsyy/site-status"
+            to="https://github.com/LddsnHong/site-status"
           />
           &nbsp;Version&nbsp;{Package.version}
         </p>
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://wsdm1.top/" text="铃铛岛少年泓" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
