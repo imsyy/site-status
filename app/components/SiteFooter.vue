f6528d3cba2995dd7319a1fbf3622045305c6844
Astrophel <99598939+Astrophel02@users.noreply.github.com>
2025-01-31 19:47:10 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/Astrophel02",
+  home: "https://www.astrophel.top",
+  email: "astrophel@astrophel.top",
 };
 </script>
 
